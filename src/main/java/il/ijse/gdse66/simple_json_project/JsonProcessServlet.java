9bccbc3083fa2f10d84ba0b7b0bca3beb0f11806
Tech-Sergent <124680474+Tech-Sergent@users.noreply.github.com>
2024-01-05 15:39:22 +0530

    servlet updated

--- src/main/java/il/ijse/gdse66/simple_json_project/JsonProcessServlet.java
+++ src/main/java/il/ijse/gdse66/simple_json_project/JsonProcessServlet.java
@@ -1,23 +1,19 @@
 package il.ijse.gdse66.simple_json_project;
 
+
 import jakarta.servlet.ServletConfig;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebInitParam;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.sql.*;
-
-@WebServlet(name = "Customer", urlPatterns = "/customers", loadOnStartup = 1, initParams = {
+@WebServlet(name = "ProcessServlet" , urlPatterns = "/test" , loadOnStartup = 0 ,initParams = {
         @WebInitParam(name = "username" , value = "root"),
         @WebInitParam(name = "password" , value = "dragon25"),
         @WebInitParam(name = "url" , value = "jdbc:mysql://localhost:3306/aad")
 })
-public class CustomerServlet extends HttpServlet {
+
+public class JsonProcessServlet extends HttpServlet {
     private String username;
     private String password;
     private String url;
@@ -30,18 +26,4 @@ public class CustomerServlet extends HttpServlet {
         password = sc.getInitParameter("password");
         url = sc.getInitParameter("url");
     }
-
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
-    }
-
-
-
-
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-
-    }
-
 }
