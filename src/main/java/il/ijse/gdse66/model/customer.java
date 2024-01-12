174700f1b01d381d9ce8e1df4d3234fdc797fe6c
Tech-Sergent <124680474+Tech-Sergent@users.noreply.github.com>
2024-01-12 10:32:06 +0530

    class created

---
+++ src/main/java/il/ijse/gdse66/model/customer.java
@@ -0,0 +1,7 @@
+package il.ijse.gdse66.model;
+
+public class customer {
+    String id;
+    String name;
+    String address;
+}
--- src/main/java/il/ijse/gdse66/simple_json_project/JsonBindServlet.java
+++ src/main/java/il/ijse/gdse66/simple_json_project/JsonBindServlet.java
@@ -3,11 +3,13 @@ package il.ijse.gdse66.simple_json_project;
 
 import jakarta.servlet.annotation.WebInitParam;
 import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
 
 @WebServlet(name = "BindingServlet" , urlPatterns = "/test2", loadOnStartup = 0 ,initParams = {
         @WebInitParam(name = "username" , value = "root"),
         @WebInitParam(name = "password" , value = "dragon25"),
         @WebInitParam(name = "url" , value = "jdbc:mysql://localhost:3306/aad")
 })
-public class JsonBindServlet {
+public class JsonBindServlet extends HttpServlet {
+
 }
