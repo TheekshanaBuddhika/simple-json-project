f67281063336e95675a1ff3fdef55e87dab6939c
Tech-Sergent <124680474+Tech-Sergent@users.noreply.github.com>
2024-01-12 10:55:40 +0530

    servlet updated

--- src/main/java/il/ijse/gdse66/model/customer.java
+++ src/main/java/il/ijse/gdse66/model/customer.java
@@ -1,7 +1,17 @@
 package il.ijse.gdse66.model;
 
-public class customer {
-    String id;
-    String name;
-    String address;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class customer implements Serializable {
+    private String id;
+    private String name;
+    private String address;
+
 }
--- src/main/java/il/ijse/gdse66/simple_json_project/JsonBindServlet.java
+++ src/main/java/il/ijse/gdse66/simple_json_project/JsonBindServlet.java
@@ -1,9 +1,17 @@
 package il.ijse.gdse66.simple_json_project;
 
 
+import il.ijse.gdse66.model.customer;
+import jakarta.json.bind.Jsonb;
+import jakarta.json.bind.JsonbBuilder;
+import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebInitParam;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
 
 @WebServlet(name = "BindingServlet" , urlPatterns = "/test2", loadOnStartup = 0 ,initParams = {
         @WebInitParam(name = "username" , value = "root"),
@@ -12,4 +20,12 @@ import jakarta.servlet.http.HttpServlet;
 })
 public class JsonBindServlet extends HttpServlet {
 
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        customer customer = new customer("C001", "kamal", "galle");
+        Jsonb jsonb = JsonbBuilder.create();
+        String s = jsonb.toJson(customer);
+        resp.getWriter().write(s);
+
+    }
 }
