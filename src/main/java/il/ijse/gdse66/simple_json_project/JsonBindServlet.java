baff22c35068bdf1b1a59f438e98e9df26cd2f68
Tech-Sergent <124680474+Tech-Sergent@users.noreply.github.com>
2024-01-12 10:28:01 +0530

    servlet updated

--- src/main/java/il/ijse/gdse66/simple_json_project/JsonBindServlet.java
+++ src/main/java/il/ijse/gdse66/simple_json_project/JsonBindServlet.java
@@ -1,4 +1,13 @@
 package il.ijse.gdse66.simple_json_project;
 
+
+import jakarta.servlet.annotation.WebInitParam;
+import jakarta.servlet.annotation.WebServlet;
+
+@WebServlet(name = "BindingServlet" , urlPatterns = "/test2", loadOnStartup = 0 ,initParams = {
+        @WebInitParam(name = "username" , value = "root"),
+        @WebInitParam(name = "password" , value = "dragon25"),
+        @WebInitParam(name = "url" , value = "jdbc:mysql://localhost:3306/aad")
+})
 public class JsonBindServlet {
 }
